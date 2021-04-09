c9fab278ed83d9670468d7e8d9f184eadcb0f5de
Ulan <80949499+Ulanka2@users.noreply.github.com>
2021-04-10 00:04:28 +0600

    ft

---
+++ dz1.8.py
@@ -0,0 +1,23 @@
+# 1
+# def func(lst):
+#     new_list = list(filter(lambda x: x % 2 == 0 and x % 3 == 0, lst))
+#     return new_list
+
+
+# numbers = map(int, input("Ведите числа через пробел:  ").split())
+# print(func(numbers))
+
+#2
+
+# lam1 = lambda x: x * 2
+# print(lam1(2))
+
+
+# 3
+# lam2 = lambda a, b: a ** b
+# print(lam2(3,3))
+
+# 4
+# lam = lambda *x: sum(x)
+
+# print(lam(10,10,10))
